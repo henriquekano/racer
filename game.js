f8bf4af991ab70447cf4f2a0284bce2f98a93997
Henrique Kano <henriquekano@hotmail.com>
2016-08-27 20:57:23 -0300

    Refactor do codigo em geral. Add limite do carro na tela.

--- game.js
+++ game.js
@@ -1,37 +1,86 @@
+"use strict";
 function bind(scope, fn) {
     return function () {
         fn.apply(scope, arguments);
     };
-}
+};
 
-var InputStatus = {
+var InputDict = {
   37: 'left',
   38: 'up',
   39: 'right',
   40: 'down',
-}
+};
 
-var Game = {};
+var Car = {
+  carHeight: 141,
+  carWidth: 93,
+  rect_x: 0,
+  rect_y: 0,
+  velocityX: 10,
+  velocityY: 10,
+  image: new Image(),
+  initCar: function(frameHeight, frameWidth){
+    this.rect_x = frameWidth / 2;
+    this.rect_y = frameHeight - this.carWidth;
+  },
+  drawCar: function(context){
+    //img
+    //x de corte
+    //y de corte
+    //largura da imagem
+    //altura da imagem
+    //x de onde colocar no canvas
+    //y ''
+    //largura da imagem final
+    //altura da imagem final
+    context.drawImage(
+      this.image, this.carWidth * (Math.ceil(this.cycles) % 4), 
+      0, this.carWidth, 141, this.rect_x, this.rect_y, this.carWidth, 141);
+  },
+  moveCar: function(right, left, up, down, frameHeight, frameWidth){
+    var newX = this.rect_x + (right ? this.velocityX : 0) - (left ? this.velocityX : 0);
+    var newY = this.rect_y + (down ? this.velocityY : 0) - (up ? this.velocityY : 0);
 
+    //Ta saindo da tela!!
+    if(newX > frameWidth - this.carWidth){
+      this.rect_x = frameWidth - this.carWidth;
+    }else if(newX < 0){
+      this.rect_x = 0;
+    }else{
+      this.rect_x = newX;
+    }
+
+    
+    if(newY > frameHeight - this.carHeight){
+      //ta saindo da tela por baixo!
+      this.rect_y = frameHeight - this.carHeight;
+    }else if(newY < 0){
+      //ta saindo da tela por cima!
+      this.rect_y = 0;
+    }else{
+      this.rect_y = newY;
+    }
+  }
+};
+Car.image.src = 'car.png';
+
+var Game = Object.create(Car);
 Game.fps = 30;
 
 
 Game.initialize = function() {
   this.entities = [];
   this.context = document.getElementById("canvas").getContext("2d");
-
-  this.car = new Image();
-  this.car.src = 'car.png';
-  this.context.drawImage(this.car, 0, 0);
-  this.carHeight = 141;
-  this.carWidth = 93;
+  
   this.frameHeight = 600;
   this.frameWidth = 800;
+  this.initCar(this.frameHeight, this.frameWidth);
+  
   //numero de ciclos rodados do jogo
   this.cycles = 0;
 
-  this.rect_x = this.frameWidth / 2;
-  this.rect_y = this.frameHeight - this.carWidth;
+  
   this.pressedKeys = {
     left: false,
     up: false,
@@ -42,16 +91,16 @@ Game.initialize = function() {
   
   var onkeydown = function(e) {
     var keyCode = e.keyCode;
-    if (InputStatus[keyCode]) {
+    if (InputDict[keyCode]) {
       e.preventDefault();
-      this.pressedKeys[InputStatus[keyCode]] = true;
+      this.pressedKeys[InputDict[keyCode]] = true;
     }
   }
   var onkeyup = function(e) {
     var keyCode = e.keyCode;
-    if (InputStatus[keyCode]) {
+    if (InputDict[keyCode]) {
       e.preventDefault();
-      this.pressedKeys[InputStatus[keyCode]] = false;
+      this.pressedKeys[InputDict[keyCode]] = false;
     }
   }
   document.addEventListener("keydown", bind(this, onkeydown), false);
@@ -63,19 +112,7 @@ Game.initialize = function() {
 Game.draw = function() {
   this.context.clearRect(0, 0, this.frameWidth, this.frameHeight);
 
-  var drawCar = function(){
-    //img
-    //x de corte
-    //y de corte
-    //largura da imagem
-    //altura da imagem
-    //x de onde colocar no canvas
-    //y ''
-    //largura da imagem final
-    //altura da imagem final
-    this.context.drawImage(this.car, 93 * (Math.ceil(this.cycles) % 4), 0, 93, 141, this.rect_x, this.rect_y, 93, 141);
-  }
-  drawCar.apply(this);
+  this.drawCar(this.context);
   // Your code goes here
  
   // =====
@@ -89,18 +126,12 @@ Game.update = function() {
   this.cycles ++;
   // =====
   // Example
-  if(this.pressedKeys.left){
-    this.rect_x = this.rect_x - 5;
-  }
-  if(this.pressedKeys.right){
-    this.rect_x = this.rect_x + 5;
-  }
-  if(this.pressedKeys.up){
-    this.rect_y = this.rect_y - 5;
-  }
-  if(this.pressedKeys.down){
-    this.rect_y = this.rect_y  + 5 ;
-  }
+  this.moveCar(this.pressedKeys.right, 
+    this.pressedKeys.left, 
+    this.pressedKeys.up, 
+    this.pressedKeys.down, 
+    this.frameHeight,
+    this.frameWidth);
   // =====
 };
 
